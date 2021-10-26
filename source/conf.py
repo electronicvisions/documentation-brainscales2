b398d759dc84315c435a459e47c2edb1c9eabe0a
Jakob Kaiser <jakob.kaiser@kip.uni-heidelberg.de>
2021-10-26 15:42:22 +0200

    Do not display "unwanted" cells in html
    compare c/15777
    
    Change-Id: Ia181c8a48dbe2248d1284fb65f35ba0c50556701

--- source/conf.py
+++ source/conf.py
@@ -53,6 +53,12 @@ html_static_path = [
     'brainscales2-demos/_static'
 ]
 
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+            'css/visions.css',
+]
+
 
 # -- Extension configuration -------------------------------------------------
 
